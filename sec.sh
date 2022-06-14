7899cd42856e0547b810d312c79505d81bcda452
techmartguru <34309846+techmartguru@users.noreply.github.com>
2022-06-14 13:48:09 +0530

    Update sec.sh

--- sec.sh
+++ sec.sh
@@ -2,4 +2,4 @@
 
 sec=`aws --region us-east-1 secretsmanager get-secret-value --secret-id db-sec|awk -F '"'  '{print $8}'|xargs`
 
-sed -i 's/secret/$sec/g' $1/db.properties
+sed -i "s/secret/$sec/g" $1/db.properties
